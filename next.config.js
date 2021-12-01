46ad9e6ab2d7478891154daccc615cdddf71dad2
Alex Lynham <alex@lynh.am>
2021-12-01 16:24:16 +0000

    Config to enable CORS for local dev

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+let config = {
+  distDir: 'dist',
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
 }
+
+// Only need rewrites for local development
+if (process.env.NEXT_PUBLIC_CHAIN_ID === 'testing') {
+  config.rewrites = async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:26657/:path*', // Proxy to Backend
+      },
+    ]
+  }
+}
+
+module.exports = withBundleAnalyzer(config)
