d69a02b79a1986f3cacb792ec916d75be115c939
Alex Lynham <alex@lynh.am>
2021-12-07 09:47:08 +0000

    Update env, remove random link from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,18 +21,6 @@ const Home: NextPage = () => {
       </div>
 
       <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 max-w-full sm:w-full">
-        <Link href="/multisig" passHref>
-          <a className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus">
-            <h3 className="text-2xl font-bold">
-              Multisigs{' '}
-              <ChevronRightIcon className="inline-block w-6 h-6 ml-2 stroke-current" />
-            </h3>
-            <p className="mt-4 text-xl">Foo.</p>
-          </a>
-        </Link>
-      </div>
-
-      <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 max-w-full sm:w-full">
         <Link href="/send" passHref>
           <a className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus">
             <h3 className="text-2xl font-bold">Send to wallet &rarr;</h3>
