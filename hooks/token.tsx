09ea76c4869d1a9c45f8587b48dff642929c84e6
zeke <30676292+ezekiiel@users.noreply.github.com>
2021-12-09 01:27:46 -0800

    Add page with view for token (#11)
    * Add manage page to view currently owned tokens
    
    * Format files with prettier:write
    
    * Add single page view for token
    
    Co-authored-by: Alex Lynham <alex@lynh.am>

---
+++ hooks/token.tsx
@@ -0,0 +1,36 @@
+import { useSigningClient } from 'contexts/cosmwasm'
+import { useEffect, useState } from 'react'
+
+export function useToken(token_id: string) {
+  const contract = process.env.NEXT_PUBLIC_WHOAMI_ADDRESS as string
+
+  const [token, setToken] = useState<string>()
+  const [loadingToken, setLoading] = useState(false)
+
+  const { signingClient } = useSigningClient()
+
+  useEffect(() => {
+    if (!signingClient) {
+      return
+    }
+
+    const getToken = async () => {
+      setLoading(true)
+      try {
+        let tokenInfo = await signingClient.queryContractSmart(contract, {
+          nft_info: {
+            token_id: token_id,
+          },
+        })
+        setToken(JSON.stringify(tokenInfo.extension, null, 4))
+        setLoading(false)
+      } catch (e) {
+        console.error(e.error)
+      }
+    }
+
+    getToken()
+  })
+
+  return { token, loadingToken }
+}
