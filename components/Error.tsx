8b68fbc94386c8d2e609b4b3d44655d1f41a5dc5
Alex Lynham <alex@lynh.am>
2022-01-04 08:31:41 +0000

    Prettify

--- components/Error.tsx
+++ components/Error.tsx
@@ -7,7 +7,6 @@ export function Error({
   errorTitle: string
   errorMessage?: string
 }) {
-
   return (
     <div
       className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
