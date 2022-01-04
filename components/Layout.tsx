1c68e2cfb5fcd977d24b0d110225384a8cdddb49
Alex Lynham <alex@lynh.am>
2022-01-04 14:55:26 +0000

    Add probably completely ineffectual t&cs #44

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react'
 import Head from 'next/head'
 import Nav from './Nav'
+import Link from 'next/link'
+import { ClipboardIcon } from '@heroicons/react/solid'
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
@@ -15,28 +17,43 @@ export default function Layout({ children }: { children: ReactNode }) {
       <main className="flex flex-col items-center justify-center w-full flex-1 p-2 md:px-20 text-center">
         {children}
       </main>
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        Powered by{' '}
-        <a
-          className="pl-1 link link-primary link-hover"
-          href="https://github.com/cosmos/cosmjs"
-        >
-          CosmJS
-        </a>
-        ,
-        <a
-          className="pl-1 link link-primary link-hover"
-          href="https://github.com/cosmoscontracts/juno"
-        >
-          Juno
-        </a>
-        <span className="pl-1"> and</span>
-        <a
-          className="pl-1 link link-primary link-hover"
-          href="https://keplr.app/"
-        >
-          Keplr
-        </a>
+      <footer className="flex flex-wrap items-center justify-center md:w-1/2 h-24 border-t">
+        <div className="flex flex-row break-after-auto pt-4">
+          Powered by{' '}
+          <a
+            className="pl-1 link link-primary link-hover"
+            href="https://github.com/cosmos/cosmjs"
+          >
+            CosmJS
+          </a>
+          ,
+          <a
+            className="pl-1 link link-primary link-hover"
+            href="https://github.com/cosmoscontracts/juno"
+          >
+            Juno
+          </a>
+          <span className="pl-1"> and</span>
+          <a
+            className="pl-1 link link-primary link-hover"
+            href="https://keplr.app/"
+          >
+            Keplr
+          </a>
+          .
+        </div>
+        <div className="text-center pb-4">
+          <p>
+            This is <strong>alpha software</strong>. By using it, you are
+            agreeing to the
+            <Link href={'/tcs'} passHref>
+              <a className="pl-1 link link-primary link-hover">
+                Terms and Conditions of use
+              </a>
+            </Link>
+            .
+          </p>
+        </div>
       </footer>
     </div>
   )
