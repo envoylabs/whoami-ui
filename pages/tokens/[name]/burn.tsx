ccc897826ccda3bbaf4a780e7554cb8ccaade747
Alex Lynham <alex@lynh.am>
2022-01-06 15:44:09 +0000

    HOTFIX: loading and errors on burn page

--- pages/tokens/[name]/burn.tsx
+++ pages/tokens/[name]/burn.tsx
@@ -7,6 +7,7 @@ import { useToken } from 'hooks/token'
 import { useRouter } from 'next/dist/client/router'
 import { TokenCard } from 'components/NameCard'
 import { Error } from 'components/Error'
+import Loader from 'components/Loader'
 import { Metadata } from 'util/types/messages'
 import { useForm } from 'react-hook-form'
 import { defaultExecuteFee } from 'util/fee'
@@ -20,6 +21,7 @@ const BurnToken: NextPage = () => {
   const contractAddress = process.env.NEXT_PUBLIC_WHOAMI_ADDRESS as string
   const { token } = useToken(tokenName, walletAddress)
   const [error, setError] = useState()
+  const [loading, setLoading] = useState(false)
 
   if (!tokenName) {
     return null
@@ -29,6 +31,8 @@ const BurnToken: NextPage = () => {
     if (!signingClient) {
       return
     }
+
+    setLoading(true)
 
     const msg = {
       burn: {
@@ -45,47 +49,58 @@ const BurnToken: NextPage = () => {
       )
       if (updatedToken) {
         router.push(`/tokens/manage`)
+        setLoading(false)
       }
     } catch (e) {
       // TODO env var for dev logging
       // console.log(e)
       setError(e.message)
+      setLoading(false)
     }
   }
 
   return (
     <WalletLoader>
-      <div className="flex flex-wrap">
-        {error && (
-          <div className="py-4">
-            <Error errorTitle={'Something went wrong!'} errorMessage={error} />
-          </div>
-        )}
-        {token ? (
-          <TokenCard name={tokenName} token={token as Metadata} />
-        ) : null}
-
-        <div className="items-center text-left m-5">
-          <h2 className="text-4xl font-bold">
-            <FireIcon className="h-9 w-9 inline mr-2 mb-1" />
-            Burn {tokenName}
-          </h2>
-          <p className="w-96">
-            This will permanently destroy the token. The token will no longer be
-            visible from the name service and another token with the same name
-            will be mintable.
-          </p>
-          <div className="p-1">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <input
-                type="submit"
-                className="btn btn-outline mt-6"
-                value="I understand, burn it"
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {error && (
+            <div className="py-4">
+              <Error
+                errorTitle={'Something went wrong!'}
+                errorMessage={error}
               />
-            </form>
+            </div>
+          )}
+          <div className="flex flex-wrap">
+            {token ? (
+              <TokenCard name={tokenName} token={token as Metadata} />
+            ) : null}
+
+            <div className="items-center text-left m-5">
+              <h2 className="text-4xl font-bold">
+                <FireIcon className="h-9 w-9 inline mr-2 mb-1" />
+                Burn {tokenName}
+              </h2>
+              <p className="w-96">
+                This will permanently destroy the token. The token will no
+                longer be visible from the name service and another token with
+                the same name will be mintable.
+              </p>
+              <div className="p-1">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <input
+                    type="submit"
+                    className="btn btn-outline mt-6"
+                    value="I understand, burn it"
+                  />
+                </form>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      )}
     </WalletLoader>
   )
 }
