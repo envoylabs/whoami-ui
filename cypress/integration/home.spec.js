06a4bf0aa72643fe15a440c4efd754427059bfc4
Danny Wilson <dannywilson32@gmail.com>
2022-01-27 09:16:16 +0000

    WIP: spying on cowmwasm signing hook

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,3 +1,6 @@
+import * as Cosmwasm from "../../hooks/cosmwasm"
+
+
 describe('Home page', () => {
   it('it should have connet wallet button on home page', () => {
     cy.visit('http://localhost:3000/')
@@ -12,5 +15,22 @@ describe('Home page', () => {
     cy.get('h3').contains('Manage')
     cy.get('h3').contains('Explore')
   })
+})
 
+describe('wallet connect', () => {
+  beforeEach(() => {
+    const spyBase = {
+      useSigningCosmWasmClient: Cosmwasm.useSigningCosmWasmClient
+    }
+
+    cy.stub(spyBase, 'useSigningCosmWasmClient').as('stubClient').returns(null)
+  })
+
+  it('it should show address if wallet connected', () => {
+
+    cy.visit('http://localhost:3000/').then(() => {
+      cy.get('@stubClient').should('have.been.called')
+    })
+
+  })
 })
