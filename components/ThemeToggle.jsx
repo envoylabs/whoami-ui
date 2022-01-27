73a4eb25c8b78c71f5ff54f3a3ed21065a07fa67
Alex Lynham <alex@lynh.am>
2022-01-27 14:18:06 +0000

    #89 better icons if not less confusing ones

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -1,6 +1,8 @@
 import { themeChange } from 'theme-change'
 import { useEffect, useState } from 'react'
 import daisyuiThemes from 'styles/daisyui-themes.json'
+import { LightBulbIcon as LightBulbIconDark } from '@heroicons/react/solid'
+import { LightBulbIcon } from '@heroicons/react/outline'
 
 const themes = Object.keys(daisyuiThemes) || ['']
 export const defaultTheme = themes[0]
@@ -17,7 +19,9 @@ function ThemeToggle() {
   return (
     <div className="form-control lg:mr-4 md:ml-auto">
       <label className="cursor-pointer label">
-        <span className="label-text">🌞</span>
+        <span className="label-text">
+          <LightBulbIcon className="h-6 w-6" />
+        </span>
         <input
           type="checkbox"
           className="toggle toggle-secondary mx-1"
@@ -29,7 +33,9 @@ function ThemeToggle() {
           }
           readOnly
         />
-        <span className="label-text">🌚</span>
+        <span className="label-text">
+          <LightBulbIconDark className="h-6 w-6" />
+        </span>
       </label>
     </div>
   )
