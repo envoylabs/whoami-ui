6825da87ddc2baffdc712b3cbbef72ac7b06d3f1
Alex Lynham <alex@lynh.am>
2022-03-12 15:39:54 +0000

    fix build

--- pages/api/ids/[name].ts
+++ pages/api/ids/[name].ts
@@ -1,10 +1,12 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { getNonSigningClient } from 'hooks/cosmwasm'
 
+const CONTRACT = process.env.NEXT_PUBLIC_WHOAMI_ADDRESS as string
+
 const getNftInfo = async (tokenId: string) => {
   const nonSigningClient = await getNonSigningClient()
   try {
-    let nftInfo = await nonSigningClient.queryContractSmart(contract, {
+    let nftInfo = await nonSigningClient.queryContractSmart(CONTRACT, {
       all_nft_info: {
         token_id: tokenId,
       },
@@ -15,16 +17,18 @@ const getNftInfo = async (tokenId: string) => {
   }
 }
 
-export default async function (req: VercelRequest, res: VercelResponse) {
+const handler = async (req: VercelRequest, res: VercelResponse) => {
   const { name } = req.query
 
   try {
-    const nftInfo = await getNftInfo(name)
+    const nftInfo = await getNftInfo(name as string)
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
-    res.json({ data })
+    res.json(nftInfo)
   } catch (error) {
     console.error(error)
     res.status(error.status || 500).end(error.message)
   }
 }
+
+export default handler
